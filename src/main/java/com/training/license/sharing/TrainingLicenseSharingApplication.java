7453a5adfc1861e6efb1bd479664d854f51b88cc
Alexandr Vacariuc <alexandr.vacariuc@endava.com>
2023-10-13 19:18:06 +0300

    Initial commit

---
+++ src/main/java/com/training/license/sharing/TrainingLicenseSharingApplication.java
@@ -0,0 +1,11 @@
+package com.training.license.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingLicenseSharingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingLicenseSharingApplication.class, args);
+    }
+}
